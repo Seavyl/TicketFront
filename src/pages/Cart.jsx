84a00a8cc93b8effc36bce7e3c66eac866f659e0
Charlene <charlene.lombardi@ik.me>
2025-06-26 12:17:17 +0200

    projet

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -2,8 +2,7 @@
 export default function Cart() {
   return (
     <div className="p-8">
-      <h1 className="text-3xl font-bold mb-4">Admin Dashboard</h1>
-      <p>Bienvenue dans le tableau de bord administrateur.</p>
+      <h1 className="text-3xl font-bold mb-4">Cart</h1>
     </div>
   );
 }
